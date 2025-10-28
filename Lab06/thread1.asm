57cf2637c09e258237d37431fecf8cef6e1aee52
JoshuaaCruz <95861337+JoshuaaCruz@users.noreply.github.com>
2025-10-27 21:36:52 -0300

    Some progress, still not working

--- Lab06/thread1.asm
+++ Lab06/thread1.asm
@@ -4,14 +4,40 @@
 #THREAD CONTADORA DE 1 SEGUNDO
 .data
 	vetor1: .word 0
-
+	msg1:	.asciiz "\nThread 1 (1s): "
 .text
 
 JThr_1:
 		
+	la		$s1, vetor1
+	li		$v0,30
+	syscall		#sycall 30 retorna tempo atual
+	addi		$s0, $a0,1000 #adicionando 1000ms ao tempo atual e salvando em s0 PARA SER PRIMEIRA MARCA
 		
-		
-		la		$s1, vetor1
-		addi		$t1,$t1,1
-		sw		$t1, 0($s1)
-		j		JThr_1	
+loopJ1:
+	li		$v0,30
+	syscall
+	
+	blt		$a0,$s0,loopJ1
+	
+	#CASO CONTADOR DE TEMPO PASSE DA MARCA
+	
+	lw		$t0,0($s1)
+	addi		$t0,$t0,1
+	sw		$t0,0($s1)
+	
+	li		$v0, 4			# Print string
+	la		$a0, msg1
+	syscall
+	
+	li		$v0, 1			# Print integer (o contador)
+	move		$a0, $t0
+	syscall
+	
+	#definindo próxima marca de 1s
+	
+	li		$v0,30
+	syscall
+	addi		$s0, $a0,1000
+	
+	j		loopJ1
--- Lab06/thread2.asm
+++ Lab06/thread2.asm
@@ -4,11 +4,39 @@
 #THREAD CONTADORA DE 3 SEGUNDO
 .data
 	vetor2: .word 0
-
+	msg2:	.asciiz "\nThread 2 (3s): "
 .text
 
 JThr_2:
-		la		$s2, vetor2
-		addi		$t1,$t1,1   #NECESSÁRIO MUDAR?
-		sw		$t1, 0($s2) #NECESSÁRIO MUDAR?
-		j		JThr_2	
+	la		$s2, vetor2
+	li		$v0,30
+	syscall		#sycall 30 retorna tempo atual
+	addi		$s3, $a0,3000 #adicionando 1000ms ao tempo atual e salvando em s3 PARA SER PRIMEIRA MARCA
+		
+loopJ2:
+	li		$v0,30
+	syscall
+	
+	blt		$a0,$s3,loopJ2
+	
+	#CASO CONTADOR DE TEMPO PASSE DA MARCA
+	
+	lw		$t0,0($s2)
+	addi		$t0,$t0,1
+	sw		$t0,0($s2)
+	
+	li		$v0, 4			# Print string
+	la		$a0, msg2
+	syscall
+	
+	li		$v0, 1			# Print integer (o contador)
+	move		$a0, $t0
+	syscall
+	
+	#definindo próxima marca de 1s
+	
+	li		$v0,30
+	syscall
+	addi		$s3, $a0,3000
+	
+	j		loopJ2	
